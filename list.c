cbca34d5f34de30dc5988ca3003e204abd7384f2
maldonadoj4atwit <maldonadoj4@wit.edu>
2025-10-24 23:47:06 -0400

    Completed function ll_head

--- list.c
+++ list.c
@@ -1,5 +1,6 @@
 #include "list.h"
 #include <stdlib.h>
+#include <stdio.h>
 // TODO: Include any necessary header files here
 
 /**
@@ -13,7 +14,12 @@ struct ll_node *ll_head(struct ll_node *head) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Returns the tail (last node) of the linked list
+ * 
+ * @param head the pointer to the first node
+ * @return the pointer to the last node of the list, 
+ * or NULL if the list is empty
  */
 struct ll_node *ll_tail(struct ll_node *head) {
     if ( head == NULL ) {
@@ -26,16 +32,15 @@ struct ll_node *ll_tail(struct ll_node *head) {
         current = current ->next ;
     }
     return current ;
-
-    {
-        /* data */
-    };
-    
-    
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Counts the number of elements in the linked list
+ *
+ * @param head the pointer to the first node of the linked list
+ * @return the total number of nodes in the list
+ * Returns 0 if the list is empty
  */
 int ll_size(struct ll_node *head) {
 
@@ -53,7 +58,13 @@ int ll_size(struct ll_node *head) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Searches for a node in the linked list that contains a given value
+ *
+ * @param head the pointer to the first node of the linked list
+ * @param value the integer value to search for in the list
+ * @return the pointer to the node that contains the specified value,
+ * or NULL if the value is not found.
  */
 struct ll_node *ll_find(struct ll_node *head, int value) {
     struct ll_node *current = head ;
@@ -71,7 +82,12 @@ struct ll_node *ll_find(struct ll_node *head, int value) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Converts the linked list into a dynamically allocated array
+ *
+ * @param head the pointer to the first node of the linked list
+ * @return the pointer to an integer array containing all node values in order
+ * or returns NULL if the list is empty
  */
 int *ll_toarray(struct ll_node *head) {
 
@@ -96,7 +112,12 @@ int *ll_toarray(struct ll_node *head) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Creates a new linked list node containing the given integer data
+ *
+ * @param data the integer value to store in the new node
+ * @return the pointer to the newly created node, 
+ * or NULL if memory allocation fails
  */
 struct ll_node *ll_create(int data) {
 
@@ -112,7 +133,10 @@ struct ll_node *ll_create(int data) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Frees all memory used by the linked list
+ *
+ * @param head the pointer to the first node of the linked list
  */
 void ll_destroy(struct ll_node *head) {
 
@@ -127,27 +151,39 @@ void ll_destroy(struct ll_node *head) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Appends a new node with the given data to the end of the linked list
+ *
+ * @param head the pointer to the first node of the linked list
+ * @param data the integer value to store in the new node
  */
 void ll_append(struct ll_node *head, int data) {
 
     struct ll_node *new_node = ll_create( data ) ;
-    if ( new_node == NULL )
+    if ( new_node == NULL ) {
         return ;
+    }
 
-    if ( head == NULL )
+    if ( head == NULL ) {
         return ;
+    }
 
     struct ll_node *tail = ll_tail( head ) ;
     tail ->next = new_node ;
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Creates a linked list from an array of integer
+ *
+ * @param data the pointer to the array of integers
+ * @param len the number of elements in the array
+ * @return the pointer to the head of the newly created linked list,
+ * or NULL if len <= 0 
  */
 struct ll_node *ll_fromarray(int* data, int len) {
 
-     if ( data == NULL || len <= 0 ){
+     if ( data == NULL || len <= 0 ) {
         return NULL ;
      }
 
@@ -172,11 +208,16 @@ struct ll_node *ll_fromarray(int* data, int len) {
 }
 
 /**
- * TODO: Describe what the function does
+ * 
+ * Removes the first node in the list that contains a specific value
+ *
+ * @param head the pointer to the first node of the linked list
+ * @param value the integer value to search for and remove
+ * @return the pointer to the head of the linked list
  */
 struct ll_node *ll_remove(struct ll_node *head, int value) {
 
-     if (head == NULL) {
+     if ( head == NULL ) {
         return NULL ;
      }
 
